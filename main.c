54965d06f6312813ac05f910906abf6762a4f132
Selim Efe <uysalselimefe@gmail.com>
2026-01-14 11:02:59 +0300

    Implement function to find the largest prime smaller than N
    Added 'Asal_Sayi_Bul' logic. The function starts from N-1 and decrements, checking each number for primality. It returns the first prime number encountered, satisfying the 'largest prime smaller than input' condition.

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int Asal_Sayi_Bul(int sayi){
+    int i,j;
+        for(j=sayi-1;j>=2;j--){
+            int asal=1;
+            for(i=2;i<j;i++){
+                if(j%i==0){
+                    asal=0;
+                    break;
+                }
+            }
+            if(asal==1){
+                break;
+            }
+        }
+        return j;
+}
+
+int main()
+{
+            printf("%d",Asal_Sayi_Bul(11));
+}
