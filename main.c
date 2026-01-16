7e897544c0149af192f34c9334283b19b9016e7a
Selim Efe <uysalselimefe@gmail.com>
2026-01-16 14:31:38 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,24 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-int Asal_Sayi_Bul(int sayi){
-    int i,j;
-        for(j=sayi-1;j>=2;j--){
-            int asal=1;
-            for(i=2;i<j;i++){
-                if(j%i==0){
-                    asal=0;
-                    break;
-                }
-            }
-            if(asal==1){
+int Asal_Sayi_Bul(int sayi)
+{
+    int i, j;
+    for (j = sayi - 1; j >= 2; j--)
+    {
+        int asal = 1;
+        for (i = 2; i < j; i++)
+        {
+            if (j % i == 0)
+            {
+                asal = 0;
                 break;
             }
         }
-        return j;
+        if (asal == 1)
+        {
+            break;
+        }
+    }
+    return j;
 }
 
 int main()
 {
-            printf("%d",Asal_Sayi_Bul(11));
+    printf("%d", Asal_Sayi_Bul(11));
 }
